a14a062756a4d96d78f852b5f8c044396c22ed4c
nikhil karkra <karkra.nikhil@gmail.com>
2019-05-05 10:41:36 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
 import * as serviceWorker from './serviceWorker';
+Amplify.configure(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
